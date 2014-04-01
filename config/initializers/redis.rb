966470ee4b26d6c8121bd80c5b7a542a2ddf92f3
David Villarama & Allen Rosario <c.allen.rosario@gmail.com>
2014-04-01 15:04:59 -0700

    fixed redis staging hostname.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 require 'redis-objects'
 
-Redis.current = Redis.new(:host => 'redis://chubs-staging.rsfqds.0001.use1.cache.amazonaws.com', :port => 6379)
+Redis.current = Redis.new(:host => 'chubs-staging.rsfqds.0001.use1.cache.amazonaws.com', :port => 6379)
